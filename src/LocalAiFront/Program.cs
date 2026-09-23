e082b0608d4d20b707cf6ebf77c21aa2361cb667
Pascal01991 <pascalino.schmidiger@gmail.com>
2026-09-23 18:20:35 +0200

    Zwischenstand Test AP07 umgesetzt aber lädt Blazor nicht

---
+++ src/LocalAiFront/Program.cs
@@ -0,0 +1,64 @@
+using Chat.Adapter.ModelRunner;
+using Chat.Core;
+using LocalAiFront.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+Konfiguration chatKonfiguration = LadeChatKonfiguration(builder.Configuration);
+chatKonfiguration.Pruefen();
+builder.Services.AddSingleton(chatKonfiguration);
+builder.Services.AddSingleton<IStore, ArbeitsspeicherStore>();
+builder.Services.AddScoped<IChatService, ChatService>();
+builder.Services.AddHttpClient<IModelServerClient, ModelRunnerClient>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
+static Konfiguration LadeChatKonfiguration(IConfiguration configuration)
+{
+    Konfiguration standard = new();
+    return new Konfiguration
+    {
+        AdresseModellserver = configuration["MODEL_RUNNER_URL"]
+            ?? configuration["Chat:AdresseModellserver"]
+            ?? string.Empty,
+        Modellname = configuration["MODEL_NAME"]
+            ?? configuration["Chat:Modellname"]
+            ?? string.Empty,
+        Systemanweisung = configuration["Chat:Systemanweisung"]
+            ?? standard.Systemanweisung,
+        Temperatur = configuration.GetValue("Chat:Temperatur", standard.Temperatur),
+        MaximaleAntwortlaenge = configuration.GetValue(
+            "Chat:MaximaleAntwortlaenge",
+            standard.MaximaleAntwortlaenge),
+        Eingabegrenze = configuration.GetValue(
+            "Chat:Eingabegrenze",
+            standard.Eingabegrenze),
+        ZeitlimitSekunden = configuration.GetValue(
+            "Chat:ZeitlimitSekunden",
+            standard.ZeitlimitSekunden),
+        SpeicherortDb = configuration["Chat:SpeicherortDb"] ?? string.Empty,
+        Protokolldatei = configuration["Chat:Protokolldatei"] ?? string.Empty
+    };
+}
