f8d453cdb1a6416575936ed99b60728f76d73c25
Minjaben <17425466+thedmo@users.noreply.github.com>
2026-09-23 08:36:39 +0200

    Changes format to windows conformant batch file

---
+++ pull_models.bat
@@ -0,0 +1,2 @@
+@REM  docker model pull hf.co/Qwen/Qwen2.5-Coder-7B-Instruct-GGUF:Q3_K_M
+docker model pull hf.co/Qwen/Qwen2.5-7B-Instruct-GGUF:Q4_K_M
--- pull_models.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-docker model pull hf.co/Qwen/Qwen2.5-Coder-7B-Instruct-GGUF:Q3_K_M
-# docker model pull hf.co/Qwen/Qwen2.5-7B-Instruct-GGUF:Q4_K_M
